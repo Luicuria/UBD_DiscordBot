599f6d075dd893958d37e7aaf5fe84fb69ceeb20
utkuberkdeniz <utkuberkdeniz@gmail.com>
2022-11-26 12:17:37 +0300

    Update 1.2

--- src/commands/musicPlayerCommands.js
+++ src/commands/musicPlayerCommands.js
@@ -76,33 +76,34 @@ function musicPlayerCmmd(client, message, args, commd, queue2) {
             return;
         }
         try {
-            var arr = new Array(20)
+            var arr = new Array(100)
             var temp
             for (let i = 0; i != -1; i++) {
                 if (typeof queue2.songs[i] === 'undefined') break;
-                //console.log(queue2.songs[i])
-                message.channel.send(`${i+1}. Song: \`${queue2.songs[i].name}\` | Duration: \`${queue2.songs[i].formattedDuration}\``)
+                arr[i] = queue2.songs[i].name
                 temp = i
             }
-            /*
             const embedmssg2 = new EmbedBuilder()
             .setTitle('Queue')
-            .setDescription(`${song.name}`)
             .setColor('#1a9c9c')
-            .addFields(
-                { name: '\u200B', value: '\u200B' },
-            )
-            .addFields(
-                { name: 'Duration', value: `\`${song.formattedDuration}\``},
-                { name: 'Requested by', value: `${song.user}`},
-            )
             .setTimestamp()
-            queue.textChannel.send({ embeds: [embedmssg2] });*/
+            console.log(arr)
+            for (let i = 0; typeof arr != -1; i++) {
+                if (typeof arr[i] === 'undefined') break;
+                if (i === 0) {
+                    embedmssg2.addFields({ name: `Song Playing:`, value: `${arr[i]}`})
+                } else {
+                    embedmssg2.addFields({ name: `${i}. Song:`, value: `${arr[i]}`})
+                }
+                
+            }
+            message.channel.send({ embeds: [embedmssg2] });
+            /*
             for (let i = 0; i != -1; i++) {
                 if (typeof queue2.songs[i] === 'undefined') break;
                 //console.log(queue2.songs[i])
                 message.channel.send(`${i+1}. Song: \`${queue2.songs[i].name}\` | Duration: \`${queue2.songs[i].formattedDuration}\``)
-            }
+            }*/
         } catch (e) {
             message.channel.send(`${e}`)
         }
